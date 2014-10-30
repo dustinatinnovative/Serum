182985c3385adf171a3fb0f43e48bafa217c61c8
Dustin Autery <dustin@dustinautery.com>
2014-10-30 14:50:53 -0500

    create doJs task

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var gulp 	= require('gulp'),
 var runSequence = require('run-sequence');
 
 /**
- * Compile Scss
- * @param  String src 	Glob of src path
- * @param  Array paths 	Paths to include for Scss @import
- * @param  String dest 	Path of stream destination
+ * doScss - Compile Scss
+ * @param  Array src 		Globs of src path.
+ * @param  Array paths 		Paths to include for Scss @import.
+ * @param  String dest 		Path of stream destination.
  * @return Object
  */
 exports.doScss = function(src, paths, dest) {
@@ -16,6 +16,23 @@ exports.doScss = function(src, paths, dest) {
 				includePaths: paths,
 				outputStyle: 'compressed'
 			}))
+		.pipe(plugins.sourcemaps.write('./maps'))
+		.pipe(gulp.dest(dest));
+};
+
+/**
+ * doJs - Compile Javascript
+ * @param  Array src		Globs of src paths.
+ * @param  String name 		Name of output file.
+ * @param  String dest 		Path of stream destination.
+ * @return Object
+ */
+exports.doJs = function(src, name, dest) {
+	return gulp.src(src)
+		.pipe(plugins.sourcemaps.init())
+			.pipe(plugins.concat( name + '.js', {newLine: ';'}))
+			.pipe(plugins.rename({ basename: name, suffix: '.min'}))
+			.pipe(plugins.uglify())
 		.pipe(plugins.sourcemaps.write('./maps'))
 		.pipe(gulp.dest(dest));
 };
