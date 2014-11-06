284e5cd13beb147e6badc661b0b2660d32686b4d
Dustin Autery <dustin@yourinnovative.com>
2014-11-06 16:13:37 -0600

    watch tasks and newer working correctly

--- index.js
+++ index.js
@@ -20,11 +20,14 @@ exports.doScss = function(src, paths, dest) {
 			.pipe(plugins.sass({
 				includePaths: paths,
 				outputStyle: 'compressed',
-				errLogToConsole: true,
-				onSuccess: function(){ console.log('SCSS compiled') }
+				errLogToConsole: true
 			}))
 		.pipe(plugins.sourcemaps.write('./maps'))
-		.pipe(gulp.dest(dest));
+		.pipe(gulp.dest(dest))
+		.pipe(plugins.notify({
+			message: '<%= file.relative %> compiled successfully',
+			notifier: function(options, callback) { }
+		}));
 };
 
 /**
@@ -41,7 +44,11 @@ exports.doJs = function(src, name, dest) {
 			.pipe(plugins.rename({ basename: name, suffix: '.min'}))
 			.pipe(plugins.uglify())
 		.pipe(plugins.sourcemaps.write('./maps'))
-		.pipe(gulp.dest(dest));
+		.pipe(gulp.dest(dest))
+		.pipe(plugins.notify({
+			message: '<%= file.relative %> compiled successfully',
+			notifier: function(options, callback) {}
+		}));
 };
 
 /**
@@ -59,14 +66,42 @@ exports.doImages = function(src, dest, options) {
 	};
 
 	return gulp.src(src)
+		.pipe(plugins.newer(dest))
 		.pipe(plugins.imagemin(options))
-		.pipe(gulp.dest(dest));
+		.pipe(gulp.dest(dest))
+		.pipe(plugins.notify({
+			message: '<%= file.relative %> minified successfully',
+			notifier: function(options, callback) {}
+		}));
 }
+
+/**
+ * doPixrem - Fallback to px unit for legacy browsers
+ * @param  Array src     	Globs of src file patterns.
+ * @param  String dest    	Path of stream destination
+ * @param  Object options 	Options for rename
+ * @return Object
+ */
+exports.doPixrem = function(src, dest, options) {
+	options = typeof options !== 'undefined' ? options : {
+		basename: 'ie8'
+	};
+
+	return gulp.src(src)
+		.pipe(plugins.newer(dest))
+		.pipe(plugins.pixrem())
+		.pipe(plugins.rename(options))
+		.pipe(gulp.dest(dest))
+		.pipe(plugins.notify({
+			message: '<%= file.relative %> pixremified successfully',
+			notifier: function(options, callback) {}
+		}));
+};
 
 /**
  * coreInstall - Do a base install of core, clean up laravel, and otherwise get ready to code!
  */
-module.exports = gulp.task('core-install', ['coreQuestions'], function() {
+exports.coreInstall = gulp.task('core-install', ['coreQuestions'], function() {
 	return runSequence('installLaravel','requireCore','composerUpdate','databaseConfig','appConfig','setMachine','setIgnores','provisionCoreDB','installBoilerplate','cleanLaravel','dump','publish');
 })
 
