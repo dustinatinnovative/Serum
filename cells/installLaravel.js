76a77bed42a5a3fdea22b50ceede2316871e3ebd
Brandon Wiele <brandon@yourinnovative.com>
2015-02-05 17:19:41 -0600

    Lock laravel project creation to ~4.2 versions.

--- cells/installLaravel.js
+++ cells/installLaravel.js
@@ -6,7 +6,7 @@ var gulp 	= require('gulp'),
 module.exports = gulp.task('installLaravel', function () {
 	return gulp.src('')
 		.pipe(shell([
-			'composer create-project --no-install --no-scripts laravel/laravel tmp',
+			'composer create-project --no-install --no-scripts laravel/laravel tmp ~4.2',
 			'mv tmp/* tmp/.[^.]* .',
 			'rm -Rf tmp'
 		]));
