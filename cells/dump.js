15b66130a3a77bba3cce7b0ef6a15d3273ee3711
Dustin Autery <dustin@yourinnovative.com>
2015-03-20 19:33:23 -0500

    remove old pieces of core installation, update package.json, pixrem default naming suffix of -ie8, eliminate sourcemap comment

--- cells/dump.js
+++
@@ -1,8 +0,0 @@
-var gulp 	= require('gulp'),
-	shell	= require('gulp-shell');
-
-// Set the local configs
-module.exports = gulp.task('dump', function () {
-	return gulp.src('')
-		.pipe(shell('php artisan dump-autoload'));
-});
